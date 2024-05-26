5ac1f5d885948f1200065b08db76ee6867313d56
Roelof Lode <roeloflode@hotmail.com>
2024-05-26 11:17:59 +0200

    initial commit

--- calculator/urls.py
+++ calculator/urls.py
@@ -4,6 +4,16 @@ from . import views
 urlpatterns = [
     
     path('', views.home, name='home'),
-    path('result/', views.result, name='result'),
     path('count/', views.count, name='count'),
+    path('result/', views.result, name='result'),
+    path('kwadraat/', views.kwadraat, name='kwadraat'),
+    path('result_kwadraat/', views.result_kwadraat, name='result_kwadraat'),
+    path('circel_omtrek/', views.circel_omtrek, name='circel_omtrek'),
+    path('result_circel_omtrek/', views.result_circel_omtrek, name='result_circel_omtrek'),
+    path('circel_oppervlakte/', views.circel_oppervlakte, name='circel_oppervlakte'),
+    path('result_circel_oppervlakte/', views.result_circel_oppervlakte, name='result_circel_oppervlakte'),
+    path('modulo/', views.modulo, name='modulo'),
+    path('result_modulo/', views.result_modulo, name='result_modulo'),
+    path('relativiteitstheorie/', views.relativiteitstheorie, name='relativiteitstheorie'),
+    path('result_relativiteitstheorie/', views.result_relativiteitstheorie, name='result_relativiteitstheorie'),
 ]
--- calculator/views.py
+++ calculator/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+import math
 
+
+# Homepage
 def home(request):
     return render(request, 'home.html')
 
+
+# Basis Calculator
+def count(request):
+    return render(request, 'count.html')
 
 def result(request):
     num1 = float(request.GET.get('number1'))
@@ -24,8 +31,79 @@ def result(request):
     return render(request, 'result.html', {'ans': ans})
 
 
-def count(request):
-    return render(request, 'count.html')
+# Kwadraat en wortel trekken
+def kwadraat(request):
+    return render(request, 'kwadraat.html')
+
+def result_kwadraat(request):
+    num1 = float(request.GET.get('number1'))
+
+    
+    if request.GET.get('kwadraat') == "":
+        ans = num1**2
+
+    else:
+        ans = math.sqrt(num1)
+
+    return render(request, 'result_kwadraat.html', {'ans': ans})
+
+
+# Circel omtrek
+def circel_omtrek(request):
+    return render(request, 'circel_omtrek.html')
+
+def result_circel_omtrek(request):
+    num1 = float(request.GET.get('number1'))
+
+    if request.GET.get('circel_omtrek') == "":
+        ans = math.pi*(num1)
+
+    return render(request, 'result_circel_omtrek.html', {'ans': ans})
+
+
+# Circel Oppervlakte
+def circel_oppervlakte(request):
+    return render(request, 'circel_oppervlakte.html')
+
+def result_circel_oppervlakte(request):
+    num1 = float(request.GET.get('number1'))
+
+    if request.GET.get('circel_oppervlakte') == "":
+        ans = math.pi*(num1)**2
+
+    return render(request, 'result_circel_oppervlakte.html', {'ans': ans})
+
+
+# Modulo
+def modulo(request):
+    return render(request, 'modulo.html')
+
+def result_modulo(request):
+    num1 = float(request.GET.get('number1'))
+    num2 = float(request.GET.get('number2'))
+
+    if request.GET.get('modulo') == "":
+        ans = num1 % num2
+
+    return render(request, 'result_modulo.html', {'ans': ans})
+
+
+# Relativiteitstheorie
+def relativiteitstheorie(request):
+    return render(request, 'relativiteitstheorie.html')
+
+def result_relativiteitstheorie(request):
+    num1 = float(request.GET.get('number1'))
+
+    if request.GET.get('relativiteitstheorie') == "":
+        ans = num1 * 300000**2
+
+    return render(request, 'result_relativiteitstheorie.html', {'ans': ans})
+
+
+
+    
+
 
 
 
